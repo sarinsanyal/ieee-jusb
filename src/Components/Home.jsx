e6de2e39719bfeaa78b879ef5f11df2fc7c23001
Shakya Majumdar <shakyamajumdar1@gmail.com>
2024-01-26 16:46:35 +0530

    make footer

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+      {/* placeholder for testing footer, remove when making this component: */}
+      <p>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/>Lorem<br/></p>
+    </div>
   )
 }
