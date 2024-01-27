4d4d386f9065231c9b3f882798e50f257a0bee65
hauntingofsam <dattashamoyita@gmai.com>
2024-01-27 19:07:31 +0530

    hero section added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import herobg from "../assets/img/12339248_945275338898078_4922629178692393455_o.jpg"
 
 export default function Hero() {
   return (
-    <div className='h-[500px]'>Hero</div>
+    <div className='bg-transparent' >
+      <img src={herobg} className='object-cover w-full h-[500px] opacity-[0.8]'/>
+      <div className='hero-title flex flex-col border-r-[10px] border-x-[#0D4C72] w-[330px] md:w-[500px] text-right pr-2 absolute top-[60%] right-[5%] font-semibold font-Josefin gap-y-4'>
+        <div className='text-4xl md:text-5xl text-black '>
+          Jadavpur University
+
+
+        </div>
+        <div className='text-4xl md:text-5xl text-[#0D4C72]'>
+          Student Branch
+
+        </div>
+      </div>
+
+      
+    </div>
   )
 }
