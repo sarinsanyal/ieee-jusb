2b462ec49df669e6bacc9b72e0a72a274cc001f0
hauntingofsam <dattashamoyita@gmai.com>
2024-01-27 19:30:06 +0530

    hero position fixed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className='bg-transparent' >
       <img src={herobg} className='object-cover w-full h-[500px] opacity-[0.8]'/>
-      <div className='hero-title flex flex-col border-r-[10px] border-x-[#0D4C72] w-[330px] md:w-[500px] text-right pr-2 absolute top-[60%] right-[5%] font-semibold font-Josefin gap-y-4'>
+      <div className='hero-title flex flex-col border-r-[10px] border-x-[#0D4C72] w-[330px] md:w-[500px] text-right pr-2 absolute top-[330px] right-[5%] font-semibold font-Josefin gap-y-4'>
         <div className='text-4xl md:text-5xl text-black '>
           Jadavpur University
 
