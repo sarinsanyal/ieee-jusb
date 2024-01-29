06a0770440400fee3166c7cac908bb37a1138eaf
Akash Bag <110753356+akash0708@users.noreply.github.com>
2024-01-29 20:35:24 +0530

    animation speed increased

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-import React from 'react';
-import './App.css'
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import Home from './Components/Home';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import Team from './Components/Team';
-import Events from './Components/Events';
+import React from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import AnimatedRoutes from "./Components/AnimatedRoutes";
 
 function App() {
-
   // const location = useLocation();
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
-        <AnimatePresence mode='wait'>
-        <Routes >
-          <Route path='/*' element={<Home/>}/>
-          <Route path='/team' element={<Team/>}/>
-          <Route path='/events' element={<Events/>}/>
-        </Routes>
-        </AnimatePresence>
-        <Footer/>
+        <Navbar />
+        <AnimatedRoutes />
+        <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/AnimatedRoutes.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Team from "./Team";
+import Events from "./Events";
+import Home from "./Home";
+import { AnimatePresence } from "framer-motion";
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/events/" element={<Events />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+export default AnimatedRoutes;
