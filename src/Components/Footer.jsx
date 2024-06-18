31f19f5a8addbb974e95345e5e9c3401786f47c1
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-06-18 15:48:12 +0530

    Footer UI has been enhanced

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -40,21 +40,135 @@ export default function Footer() {
             <div className='grid_icon'>
               <h4 className="text-xl font-extrabold mb-3 underline-hover">Follow Us</h4>
               <div className="grid grid-cols-2 gap-2 mt-4 mr-[156px] icons">
-                <a href="https://twitter.com/IeeeJadavpur" className="w-8 h-8" target="blank"><BsTwitterX className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" /></a>
-                <a href="https://www.facebook.com/ieeejusb" className="w-8 h-8" target="blank"><FaFacebook className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" /></a>
-                <a href="https://www.instagram.com/_ieeeju/" className="w-8 h-8" target="blank"><FaInstagram className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" /></a>
-                <a href="https://www.linkedin.com/company/ieee-ju" className="w-8 h-8" target="blank"><FaLinkedin className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" /></a>
+                <a
+                  href="https://twitter.com/IeeeJadavpur"
+                  className="w-8 h-8"
+                  target="blank"
+                >
+                  <BsTwitterX className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
+                <a
+                  href="https://www.facebook.com/ieeejusb"
+                  className="w-8 h-8"
+                  target="blank"
+                >
+                  <FaFacebook className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
+                <a
+                  href="https://www.instagram.com/_ieeeju/"
+                  className="w-8 h-8"
+                  target="blank"
+                >
+                  <FaInstagram className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/ieee-ju"
+                  className="w-8 h-8"
+                  target="blank"
+                >
+                  <FaLinkedin className="h-8 w-8 transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
               </div>
             </div>
           </div>
+        </div>
+        <div className="mx-auto h-[2px] w-[90vw] bg-white rounded"></div>
+        <div className="flex flex-col justify-between items-center text-sm mt-3 h-[55px]">
+          <p className="mt-2 mb-2">
+            © Copyright IEEE-JUSB. All Rights Reserved
+          </p>
+          <p className="mb-5">
+            Made with <FaHeart className="inline text-red-500 mx-1" /> by IEEE
+            JUSB
+          </p>
+        </div>
+      </footer>
 
-          <div className="flex flex-col justify-between items-center text-sm mt-4 h-[55px] border-t-2 border-solid border-orange-100">
-            <p className='mt-2 mb-2'>© Copyright IEEE-JUSB. All Rights Reserved</p>
-            <p>Made with <FaHeart className="inline text-red-500 mx-1" /> by IEEE JUSB</p>
+
+      <footer className="bg-sky-950 text-white font-Josefin pb-5 pt-7 mt-5">
+        <div className="footer-grid mb-4 w-[90vw]  mx-auto gap-[15px] lg:gap-[20px]  ">
+          <div className="footer-div">
+            <h4 className="footer-header">IEEE JUSB</h4>
+            <p className="text-[16px] list-items">
+              The Jadavpur University IEEE student branch, founded in 2010,
+              belongs to the Kolkata section of Region 10 of the organization.
+              We are a group of enthusiastic students who are promoting
+              innovative ideas both within and outside the campus.
+            </p>
           </div>
+          <div className="footer-div">
+            <h4 className="footer-header">Useful Links</h4>
+            <div className="list-div ">
+              <a href="/" className="list-items hover-link">
+                Homes
+              </a>
+              <a href="/#about" className="list-items hover-link">
+                About Us
+              </a>
+              <a href="/#chapter  " className="list-items hover-link">
+                Chapters
+              </a>
+              <a href="/events" className="list-items hover-link">
+                Events
+              </a>
+              <a href="/#sponsor" className="list-items hover-link">
+                Sponsors
+              </a>
+              <a href="/team" className="list-items hover-link">
+                Team
+              </a>
+            </div>
+          </div>
+          <div className="footer-div">
+            <h4 className="footer-header">Contact Us</h4>
+            <p className="list-items">
+              Jadavpur University 188, Raja S.C. Mallick Rd, Kolkata-700032
+              India
+            </p>
+            <p className="list-items">Phone: +91 98361 05125</p>
+            <p className="list-items">Email: <a href="mailto:jaduniv.ieee@gmail.com">jaduniv.ieee@gmail.com</a></p>
+          </div>
+          <div className="footer-div">
+            <h4 className="footer-header">Follow Us</h4>
+            <div className="grid grid-cols-4 items-center place-items-center max-w-[220px] mx-auto lg:place-items-start lg:mx-0 ">
+                <a
+                  href="https://twitter.com/IeeeJadavpur"
+                  target="blank"
+                >
+                  <BsTwitterX className="footer-icons transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
+                <a
+                  href="https://www.facebook.com/ieeejusb"
+                  target="blank"
+                >
+                  <FaFacebook className="footer-icons transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
+                <a
+                  href="https://www.instagram.com/_ieeeju/"
+                  target="blank"
+                >
+                  <FaInstagram className="footer-icons transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/ieee-ju"
+                  target="blank"
+                >
+                  <FaLinkedin className="footer-icons transition-colors duration-150 ease-in-out hover:text-blue-700" />
+                </a>
+              </div>
+          </div>
+        </div>
+        <div className="mx-auto h-[2px] w-[90vw] bg-white rounded"></div>
+        <div className="flex flex-col justify-between items-center text-sm mt-3 h-[55px]">
+          <p className="mt-2 mb-2">
+            © Copyright IEEE-JUSB. All Rights Reserved
+          </p>
+          <p className="mb-5">
+            Made with <FaHeart className="inline text-red-500 mx-1" /> by IEEE
+            JUSB
+          </p>
         </div>
       </footer>
     </div>
   );
 }
-
