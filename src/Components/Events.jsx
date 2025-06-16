2b30b0b946fe8e7fc3ae344cfe3902e56bf60a3b
dasakash26 <akashd2664@gmail.com>
2025-06-16 09:38:28 +0530

    Remove logs

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -10,15 +10,11 @@ export default function Events() {
   const [itemOffset, setItemOffset] = useState(0);
 
   const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
   const currentItems = items.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(items.length / itemsPerPage);
 
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
 
     const eventsSection = document.querySelector(".events-container");
 
