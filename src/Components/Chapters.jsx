4be35afc10dcbba7927f7771791018c8d848442f
sarinsanyal <sarinsanyal2005@gmail.com>
2025-06-16 18:09:45 +0530

    Added Chapter names

--- src/Components/Chapters.jsx
+++ src/Components/Chapters.jsx
@@ -21,19 +21,20 @@ export default function Chapters() {
         </div>
         <div className="md:w-1/3 py-10 md:py-0 flex flex-col items-center md:justify-end md:h-64">
           <img src="/chapters/pes.png" alt="" className="mb-5"/>
-          <p className="font-bold text-center text-2xl">Power & Energy Society <br /> Students Branch Chapter, <br /> C16</p>
+          <p className="font-bold text-center text-2xl">Power & Energy Society <br /> Students Branch Chapter, <br /> PE31</p>
         </div>
         <div className="md:w-1/3 py-10 md:py-0 flex flex-col items-center md:justify-end md:h-64">
           <img src="/chapters/wie.png" alt="" className="mb-5"/>
-          <p className="font-bold text-center text-2xl">Women in Engineering <br /> Students Branch Chapter, <br /> C16</p>
+          <p className="font-bold text-center text-2xl">Women in Engineering <br /> Students Branch Chapter, <br /> WIE</p>
         </div>
         <div className="md:w-1/3 py-10 md:py-0 flex flex-col items-center md:justify-end md:h-64">
           <img src="/chapters/comsoc.png" alt="" className="mb-5"/>
-          <p className="font-bold text-center text-2xl">Communication Society <br /> Students Branch Chapter, <br /> IA34</p>
+          <p className="font-bold text-center text-2xl">Communication Society <br /> Students Branch Chapter, <br /> COM19</p>
         </div>
         <div className="md:w-1/3 py-10 md:py-0 flex flex-col items-center md:justify-end md:h-64">
           <img src="/chapters/sps.png" alt="" className="mb-5" height={220} width={220}/>
-          <p className="font-bold text-center text-2xl">Signal Processing Society <br /> Students Branch Chapter, <br /> IA34</p>
+          <p className="font-bold text-center text-2xl">Signal Processing Society <br /> Students Branch Chapter,<br /> SPS </p>
+          
         </div>
       </div>
     </div>
